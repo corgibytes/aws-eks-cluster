7be335488e8883b80c91eb023de92fced74035ea
M. Scott Ford <scott@corgibytes.com>
2023-12-15 17:25:02 -0500

    Marks this branch as a fork of the upstream project

--- core-services/core_services.tf
+++ core-services/core_services.tf
@@ -74,7 +74,7 @@ module "ingress_nginx" {
 
 module "external_dns" {
   count                = local.enable_external_dns ? 1 : 0
-  source               = "github.com/massdriver-cloud/terraform-modules//k8s-external-dns-aws?ref=b4c1dda"
+  source               = "github.com/corgibytes/terraform-modules//k8s-external-dns-aws"
   kubernetes_cluster   = local.kubernetes_cluster_artifact
   md_metadata          = var.md_metadata
   release              = "external-dns"
